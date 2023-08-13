b31321be70c888425df84a1e67b6ccc64a11f830
Damola Olusakin <olusakinadedamola@gmail.com>
2023-08-12 23:42:17 -0400

    first commit

---
+++ containers-lab.py
@@ -0,0 +1,19 @@
+# exercise 1
+# Create a list named students containing some student names (strings).
+# Print out the second student's name.
+# Print out the last student's name.
+
+students = ['David', 'John', 'Jonah', 'Rick', 'Obama']
+
+print(students[1])
+print(students[-1])
+
+
+# Exercise 2
+# Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
+# Use a for loop to print out the string "[food goes here] is a good food".
+
+foods = ('orange', 'apple', 'pear', 'mango', 'banana',)
+
+for food in foods:
+    print(f"{food} is a good food")
